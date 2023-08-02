363a12061eaee8e9bd930dd5f5f25a69b8bb3212
devchadha-jmi <chadhadev2002@gmail.com>
2023-08-02 20:35:36 +0530

    Updated FIFO Logic, works fine, regression test pending

--- FIFO_design/FIFO_router.v
+++ FIFO_design/FIFO_router.v
@@ -1,181 +1,202 @@
-module fifo_router#(
-  parameter width = 8,
-  parameter depth = 16)
-  (input 	clock,						// Same clock to read and write, no CDC
-   input 	resetn,
-   input 	soft_reset,
-   input 	write_enb,
-   input 	read_enb,
-   input 	lfd_state,
-   input 	[width-1:0] data_in,
-   output   [width-1:0] data_out,
-   output	reg full,
-   output	reg	empty);
+module fifo_min_project#(parameter width = 9,
+                         parameter depth = 16)(
+  input 	clock,					// Synchronous FIFO
+  input 	resetn,					// Active Low resetn
+  input 	soft_reset,				// Active high soft_reset
+  input 	write_enb,
+  input 	read_enb,
+  input 	[width-1:0]data_in,
+  output 	[width-1:0]data_out,
+  output reg full,
+  output reg empty
+);
   
-  // Internal signals
-  // Read and write pointer decides the write and read locations
-  reg [3:0]rd_ptr;
-  reg [3:0]wr_ptr;
-  // fifo counter drives full and empty flags
-  reg [3:0]fifo_counter;
+  // Internal Signals for FIFO to function---------------
+  reg [4:0] rd_ptr;     // Fifo depth is 16, pointer of 4 bit would be sufficient,
+  reg [4:0] wr_ptr;		// but 4'b0 is untouched state, 
+  						// so counter counts from 5'b00000 till 5'b10000
+  
+  // Fifo counter drives full and empty flags
+  reg [4:0] fifo_counter;
   
   // Read and Write Logics
-  wire READ  = !empty && read_enb;
-  wire WRITE = !full  && write_enb;
+  wire READ  = ~(~resetn || soft_reset) &&!empty && read_enb;
+  wire WRITE = ~(~resetn || soft_reset) &&!full  && write_enb;
   
   // Defining FIFO Memory
   // 9 bit wide fifo with a depth 16
+  reg [width-1:0]FIFO[depth-1:0];
   
-  reg [8:0]FIFO[15:0];
+  // Temporary Data Out for adding timer logic to the IP
+  reg [8:0]data_out_temp;
   
-  // Flag Driver-----------------------------------------------------------------------------
-  always @ (fifo_counter)
+  // FLAGS Driving Logic ---------------------------------
+  always @ (*)
     begin
-      empty <= (fifo_counter == 4'b0000);
-      full  <= (fifo_counter == 4'b1111);
+      empty <= (fifo_counter == 0);
+      full  <= (fifo_counter == depth);
     end
   
-  // fifo_counter driver----------------------------------------------------------------------
-  
+  // fifo_counter driver----------------------------------
   always @ (posedge clock)
     begin
       // fifo counter resets to 0 on reset being invoked
       if (~resetn | soft_reset)
-        fifo_counter <= 4'b0;
+        fifo_counter <= 'b0;
       
       // fifo counter does not change on simultaneous read and write
       else if (READ && WRITE)
         fifo_counter <= fifo_counter;
       
       // fifo counter decrements on read
-      else if (READ)
+      else if (READ && ~WRITE)
         fifo_counter <= fifo_counter-1;
       
       // fifo counter increments on write
-      else if (WRITE)
+      else if (~READ && WRITE)
         fifo_counter <= fifo_counter+1;
       
       else
         fifo_counter <= fifo_counter;   
     end
   
-  // Memory READ and WRITE-----------------------------------------------------------------
-  
-  reg [8:0]data_out_9bit;
-  
+  // Memory READ and WRITE--------------------------------
   always @ (posedge clock)
     begin
       // Data out is 0 when reset
       if (~resetn | soft_reset)
-        data_out_9bit <= {width{1'b0}};
+        begin
+          for(int i = 0; i < 16; i++)
+            begin
+              FIFO[i] <= 'b0;
+            end
+        end
       
       // Simultaneous Read and Write
       else if (READ && WRITE)
         begin
-          FIFO[wr_ptr]  <= {lfd_state, data_in};
-          data_out_9bit <= FIFO[rd_ptr];
+          FIFO[wr_ptr]  <= data_in;
+          data_out_temp <= FIFO[rd_ptr];
         end
       
       // Only READ
-      else if (READ)
-        data_out_9bit <= FIFO[rd_ptr];
+      else if (READ && ~WRITE)
+        begin
+          FIFO[wr_ptr]  <= FIFO[wr_ptr];
+          data_out_temp <= FIFO[rd_ptr];
+        end
       
       // Only WRITE
-      else if (WRITE)
-        FIFO[wr_ptr]  <= {lfd_state, data_in};
-      
+      else if (~READ && WRITE)
+        begin
+          FIFO[wr_ptr]  <= data_in;
+          data_out_temp <= 'b0;
+        end
+
       else
         begin
           FIFO[wr_ptr]  <= FIFO[wr_ptr];
-          data_out_9bit <= 9'b0;
+          data_out_temp <= 'b0;
         end   
     end
   
-      // Data out is in High Impedance state for two scenarios
-      // 1. When FIFO is empty
-      // 2. When FIFO is in timeout stage
-  	  // TIME-OUT - When counter goes to zero
-      // Counter starts when lfd-state is 1
-  
-  // State Machine for Counter control
-  // /*Three*/ Two states
-  // 1. Counter Load
-  // 2. Counter Decrement
-  
-  parameter counter_load = 2'b01;    // Value loaded to counter when lfd_state is 1
-  parameter counter_dcr  = 2'b10;	 // Counter decrements after loading
-  
-  reg [6:0]counter;
-  reg [1:0]state, next_state;
-  
-  always @ (*)
-    begin
-      case (state)
-        counter_load: begin
-          next_state <= (data_out_9bit[0]) ? counter_dcr : counter_load;
-          counter <= (data_out_9bit[0]) ? data_out_9bit[8:3] + 1'b1 : 7'b0;
-        end
-        counter_dcr : begin 
-          next_state <= (counter == 7'b0) ? counter_load : counter_dcr;
-          //counter <= (counter != 7'b0) ? counter - 1'b1 : counter;
-        end
-      endcase
-    end
-  
-  always @ (posedge clock)
-    begin
-      if (~resetn | soft_reset)
-        state <= counter_load;
-      else
-        state <= next_state;
-      
-      // Synchronous decrement
-        counter <= (counter != 7'b0) ? counter - 1'b1 : counter;
-    end
-  
-  /* Load Counter
-  always @ (posedge clock)
-    begin
-      if (~reset | soft_reset)
-        counter <= 7'b0;
-      
-      else if (data_out_9bit[0])				// Load counter when lfd state is 1
-        counter <= data_out_9bit[8:3] + 1'b1;
-      
-      else if (counter == 7'b0)					// When counter is 0 it stays at 0
-        counter <= counter;
-      
-      else										// Else counter always decrements
-        counter <= counter - 1'b1;
-    end
-    
-    */
-  
-  assign data_out = (counter == 7'b0 | empty) ? 8'bz : data_out_9bit[8:1];  
-  
-  // rd_ptr and wr_ptr update--------------------------------------------------------------
+  // rd_ptr and wr_ptr update--------------------------------
   always @ (posedge clock)
     begin
       if (~resetn | soft_reset)
         begin
-          rd_ptr <= 4'b0;
-          wr_ptr <= 4'b0;
+          rd_ptr <= 'b0;
+          wr_ptr <= 'b0;
         end
+      
       else if (READ && WRITE)
         begin
-          rd_ptr <= rd_ptr + 4'b1;
-          wr_ptr <= wr_ptr + 4'b1;
+          rd_ptr <= rd_ptr + 'b1;
+          wr_ptr <= wr_ptr + 'b1;
         end
-      else if (READ)
-        rd_ptr <= rd_ptr + 4'b1;
-      else if (WRITE)
-        wr_ptr <= wr_ptr + 4'b1;
-      else
+      
+      else if (READ && ~WRITE)
+        begin
+          rd_ptr <= rd_ptr + 'b1;
+          wr_ptr <= wr_ptr;
+        end
+        
+      else if (~READ && WRITE)
+        begin
+          wr_ptr <= wr_ptr + 'b1;
+          rd_ptr <= rd_ptr;
+        end
+      
+      else if (~READ && ~WRITE)
         begin
           wr_ptr <= wr_ptr;
           rd_ptr <= rd_ptr;
         end
     end
-
+  
+  // Instantiate Timer module to main design block
+  wire out_disable;    // Time out situation
+  wire load_timer = data_out_temp[0] | 1'b0;
+  Timer dut (.clock(clock),
+             .resetn(resetn),
+             .READ(READ),
+             .soft_reset(soft_reset),
+             .load_timer(load_timer),
+             .data(data_out_temp[width-1:width-6]),
+             .time_out(out_disable)
+            );
+  
+  // Assign data_out
+  assign data_out = (out_disable || data_out_temp == 'b0) ? 'bz: data_out_temp;
 endmodule
-                   
+
+module Timer (input clock,
+              input resetn,
+              input READ,
+              input soft_reset,
+  			  input load_timer,
+              input [5:0]data,    // Upper 6 MSBs of Data_out responsible for loading counter
+              output time_out     // Active high disable signal
+             );
+  // State Definition
+  parameter idle = 2'b00,
+  			out_enb = 2'b01,
+  			first_byte = 2'b10;
+  
+  reg [1:0]state, next_state;
+  
+  always @ (posedge clock)
+    begin
+      if (~resetn || soft_reset)
+        state <= idle;
+      else
+        state <= next_state;
+    end
+  
+  always @ *
+    begin
+      case (state)
+        idle:    	next_state = (READ) ? first_byte : idle;
+        first_byte: next_state = (READ && load_timer) ? out_enb : idle;
+        out_enb: 	next_state = (counter == 7'b0) ? idle : out_enb;
+        default  	next_state = idle;
+      endcase
+    end
+        
+  reg [6:0]counter;
+  
+  always @ (posedge clock)
+    begin
+      if (~resetn || soft_reset)
+        counter <= 7'b0;
+      else if (load_timer)
+        counter <= data + 1'b1;
+      else if (counter != 7'b0 && (state == out_enb))
+        counter <= counter - 1'b1;
+      else
+        counter <= counter;
+    end
+  
+  assign time_out = (state == idle || (state == first_byte && ~load_timer)) ? 1'b1 : 1'b0;
+  
+endmodule
--- FIFO_design/fifo_router_tb.v
+++ FIFO_design/fifo_router_tb.v
@@ -1,34 +1,26 @@
 module fifo_router_tb;
 
-  // Parameters
-  parameter width = 8;
-  parameter depth = 16;
-
   // Inputs
-  reg clock = 0;
+  reg clock = 1;
   reg resetn;
   reg soft_reset;
   reg write_enb;
   reg read_enb;
   reg lfd_state;
-  reg [width-1:0] data_in;
+  reg [8:0] data_in;
 
   // Outputs
-  wire [width-1:0] data_out;
+  wire [8:0] data_out;
   wire full;
   wire empty;
 
   // Instantiate the FIFO Router
-  fifo_router #(
-    .width(width),
-    .depth(depth)
-  ) dut (
+  fifo_min_project dut (
     .clock(clock),
     .resetn(resetn),
     .soft_reset(soft_reset),
     .write_enb(write_enb),
     .read_enb(read_enb),
-    .lfd_state(lfd_state),
     .data_in(data_in),
     .data_out(data_out),
     .full(full),
@@ -38,93 +30,50 @@ module fifo_router_tb;
   // Clock Generation
   always #5 clock = ~clock;
   
-    // VCD Files
-   // initial begin
-   // $dumpfile("dump.vcd");
-   // $dumpvars(1);
-  //end
+   // VCD Files
+    initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
 
-  // Initialize Inputs
+  // Tests
   initial begin
-    resetn = 0;
-    soft_reset = 1;
-    write_enb = 0;
-    read_enb = 0;
-    lfd_state = 0;
-    data_in = 0;
+    // Testing resetn and soft_reset
+    resetn <= 0;
+    soft_reset <= 1;
     #10;
-    resetn = 1; // Release reset
-    #50;
-    soft_reset = 0; // Deactivate soft reset
+    resetn <= 1;
+    soft_reset <= 0;
+    #10;
+    // Testing READ and WRITE
+    write_enb = 1;
+    read_enb = 0;
+    data_in = 9'b000110101;
+    #10;
+    for (int i = 0; i < 20; i++)
+      begin
+        //data_in = $random%512; 	// This can be tough to test
+        						 	// unless we have a 
+        						 	//self-testing testbench
+        data_in = 9'b101010100;
+        #10;
+      end
+    write_enb = 0;
+    #5;
+    read_enb = 1;
+    #220;
+    read_enb = 0;
   end
-
-  // Stimulus
+  
+  
   initial begin
-    // Test 1: Write some data into FIFO
-    write_enb = 1;
-    data_in = 8'b10101010;
-    #50;
-    write_enb = 1;
-
-    // Test 2: Read data from FIFO
-    read_enb = 1;
-    #30;
-    read_enb = 1;
-
-    // Test 3: Test time-out feature
-    lfd_state = 1; // Enable time-out
-    data_in = $urandom%256; // Load 6 MSBs of timer, LSB doesn't matter for now
-    #50;
-    data_in = $urandom%256; // Reload the timer (counter_load)
-    #120; // Wait for some clock cycles
-    lfd_state = 1; // Disable time-out
-    #200;
-
-    // Test 4: Fill the FIFO
-    for (int i = 0; i < depth; i = i + 1) begin
-      write_enb = 1;
-      lfd_state = 1;
-      data_in = $urandom%256; // Writing consecutive numbers to the FIFO
-      #20;
-    end
-    write_enb = 1;
-
-    // Test 5: Read from full FIFO
-    for (int i = 0; i < depth; i = i + 1) begin
-      read_enb = 1;
-      #20;
-      read_enb = 1;
-    end
-
-    // Test 6: Empty the FIFO
-    for (int i = 0; i < depth; i = i + 1) begin
-      write_enb = 0;
-      lfd_state = 1;
-      data_in = $urandom%256;
-      #20;
-      write_enb = 1;
-    end
-
-    // Test 7: Read from empty FIFO
-    for (int i = 0; i < depth; i = i + 1) begin
-      read_enb = 1;
-      #20;
-      read_enb = 1;
-    end
-
-    // Test 8: Test reset functionality
-    resetn = 0;
-    #30;
-    resetn = 1;
-    #100;
-
-    $display("Exhaustive test completed.");
-    $finish; // End simulation
+    #450;
+    $finish();
   end
-
+  
   // Monitor
   always @(posedge clock) begin
-    $display("FIFO State: Full=%b, Empty=%b, Counter=%b, write_enb=%b, lfd_state=%b, data_in=%b, read_enb=%b, data_out=%b", full, empty, dut.fifo_counter, write_enb, lfd_state, data_in, read_enb, data_out);
+    $display("FIFO State: Full=%b, Empty=%b, Counter=%b, write_enb=%b, data_in=%b, read_enb=%b, data_out=%b, rd_ptr=%b", full, empty, dut.fifo_counter, write_enb, /*lfd_state,*/ data_in, read_enb, data_out/*, dut.counter*/, dut.rd_ptr);
   end
 
 endmodule
